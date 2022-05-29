13262cc8b18f19e44b658ee54a733960b55dd08c
Dale Glass <dale@daleglass.net>
2022-05-29 02:09:55 +0200

    Support building in both Ubuntu 20.04 and 22.04
    Improve build script to use a filesystem with the most free space as temporary
    storage, since the containers get big.

--- build_container.sh
+++ build_container.sh
@@ -1,2 +1,38 @@
 #!/bin/bash
-podman build . -t webrtc-builder:20.04
+EXTRA_ARGS="$@"
+
+[ -z "$WEBRTC_CONTAINER" ] && WEBRTC_CONTAINER=ubuntu-20.04
+
+
+
+# All this stuff is here because by default /var/tmp gets used, and it may not big
+# enough on some systems to hold the temporary data. So we look for places with room.
+#
+# We avoid tmpfs because it might cause memory exhaustion.
+
+most_space=0
+most_space_dir=
+
+for temp_dir in /tmp /var/tmp $HOME/tmp ; do
+	filesystem=`awk "\\$2 == \"$temp_dir\" {print \\$3}" /proc/mounts`
+#	echo "Filesystem for $temp_dir: $filesystem"
+
+	if [ "$filesystem" != "tmpfs" ] ; then
+		space=`df -k --output=avail "$temp_dir" | tail -n 1`
+#		echo "Space in $temp_dir: $space"
+
+		if [ "$space" -gt "$most_space" ] ; then
+			most_space=$space
+			most_space_dir=$temp_dir
+		fi
+	fi
+done
+
+echo "Setting temp dir to location with most free space: $most_space_dir"
+
+export TEMPDIR=$most_space_dir
+export TMPDIR=$TEMPDIR
+
+echo "Building $WEBRTC_CONTAINER"
+echo ""
+podman build -f Dockerfile.$WEBRTC_CONTAINER $EXTRA_ARGS . -t webrtc-builder:$WEBRTC_CONTAINER
--- shell_in_container.sh
+++ shell_in_container.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+[ -z "$WEBRTC_CONTAINER" ] && WEBRTC_CONTAINER=ubuntu-20.04
 
 $SCRIPTPATH/build_container.sh
+
+
+echo "Running shell in $WEBRTC_CONTAINER"
+echo ""
 podman run -it  \
 	-v $SCRIPTPATH/patches:/webrtc-build/patches:Z \
 	-v $SCRIPTPATH/scripts:/webrtc-build/scripts:Z \
 	-v $SCRIPTPATH/out:/webrtc-build/out:Z \
-	 webrtc-builder:20.04 
+	 webrtc-builder:$WEBRTC_CONTAINER
